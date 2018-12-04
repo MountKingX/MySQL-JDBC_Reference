4bcf8ae5736881be130969cc40ae98796492f571
Kangmin Xie <kangmin.xie@gmail.com>
2018-12-03 21:31:50 -0600

    better formatted

--- MySQLJavaCode/src/ConnectionPoolSetup.java
+++ MySQLJavaCode/src/ConnectionPoolSetup.java
@@ -6,71 +6,71 @@ import java.util.List;
 
 public class ConnectionPoolSetup {
 
-	//private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";//deprecated
-	private static final String USERNAME = "";
-	private static final String PASSWORD = "";
-	private static final String DATABASE = "test";
-	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/" + DATABASE + "?useSSL=false";
-	// connection pool
-	private static List<Connection> connectionPool = new ArrayList<Connection>();
-	//private static JSONParser parser = new JSONParser();
-	
+    // private static final String JDBC_DRIVER ="com.mysql.jdbc.Driver";//deprecated
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+    private static final String DATABASE = "test";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/" + DATABASE + "?useSSL=false";
+
+    // connection pool
+    private static List<Connection> connectionPool = new ArrayList<Connection>();
+    // private static JSONParser parser = new JSONParser();
+
+    /**
+     * method to get a mysql connection from connection pool
+     * @return one Connection
+     * @throws Exception
+     */
     private static synchronized Connection getConnection() throws Exception {
-        if (connectionPool.size() > 0) {
-            return connectionPool.remove(connectionPool.size() - 1);
-        }
-        // get class Name
-        
-/*      deprecated
- * 		try {
-            Class.forName(JDBC_DRIVER); //System.out.println("GET CLASS");
-        } catch (ClassNotFoundException e) {
-            throw new Exception(e);
-        }*/
-        
-        // get Connection
-        try {  //System.out.println("GET CONNECION");
-            return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            throw new Exception(e);
-        }
-    }
-    
-    private static synchronized void releaseConnection(Connection con) {
-        connectionPool.add(con);
-    }
+	// re-use a previous released connection
+	if (connectionPool.size() > 0) {
+	    return connectionPool.remove(connectionPool.size() - 1);
+	}
 	
-    public static void main(String[] args) throws SQLException {
-    	Connection con = null;
-    	try {
-    		con = getConnection();
-    		System.out.println("Connection established");
-    		
-    		
-    		// operation for DB
-    		
-    		//CRUD
-    		
-    		// create
-    		// read
-    		// update
-    		// delete
-    		
-    		// statement vs prepared-statement
-    		
-    		
-    	} catch (Exception e) {
-        	//System.out.println("exception got");
-			e.printStackTrace();
-		} finally {
-			releaseConnection(con);
-    		connectionPool.clear();
-        	if (con != null) {
-        		con.close();
-        		System.out.println("Closed the connection successful!!");
-        	}
-		}
+	// get class Name
+	/*
+	 * deprecated try { Class.forName(JDBC_DRIVER);
+	 * //System.out.println("GET CLASS"); } catch (ClassNotFoundException e) { throw
+	 * new Exception(e); }
+	 */
+
+	// get Connection
+	try { // System.out.println("GET CONNECION");
+	    return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+	} catch (SQLException e) {
+	    throw new Exception(e);
+	}
     }
-    
-    
+
+    // release current connection to pool when leaving
+    private static synchronized void releaseConnection(Connection con) {
+	connectionPool.add(con);
+    }
+
+    public static void main(String[] args) throws SQLException {
+	Connection con = null;
+	try {
+	    con = getConnection();
+	    System.out.println("Connection established");
+
+	    // operation for DB
+	    // CRUD
+	    // create
+	    // read
+	    // update
+	    // delete
+	    // statement vs prepared-statement
+
+	} catch (Exception e) {
+	    // System.out.println("exception got");
+	    e.printStackTrace();
+	} finally {
+	    releaseConnection(con);
+	    connectionPool.clear();
+	    if (con != null) {
+		con.close();
+		System.out.println("Closed the connection successful!!");
+	    }
+	}
+    }
 }
--- MySQLJavaCode/src/CreationOfTable.java
+++ MySQLJavaCode/src/CreationOfTable.java
@@ -8,19 +8,23 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * JDBC connection sample
+ * @author kangmin
+ *
+ */
 class User {
-	private int uid;
+    private int uid;
     private String userName;  // unique email address
     private String password;
     private String firstName;
     private String lastName;
-    
-    public int getUid()        		   { return uid;       }
-    public String getPassword()        { return password;  }
-    public String getUserName()        { return userName;  }
-    public String getFirstName()       { return firstName; }
-    public String getLastName()        { return lastName;  }
+
+    public int getUid()			{ return uid;       }
+    public String getPassword()        	{ return password;  }
+    public String getUserName()        	{ return userName;  }
+    public String getFirstName()       	{ return firstName; }
+    public String getLastName()        	{ return lastName;  }
 
     public void setUid(String s)       { password = s;  }
     public void setPassword(String s)  { password = s;  }
@@ -32,110 +36,108 @@ class User {
 
 public class CreationOfTable {
 
-	private static final String USERNAME = "";
-	private static final String PASSWORD = "";
-	private static final String DATABASE = "test";
-	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/" + DATABASE + "?useSSL=false";
-	private static List<Connection> connectionPool = new ArrayList<Connection>();
-	
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+    private static final String DATABASE = "test";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/" + DATABASE + "?useSSL=false";
+    private static List<Connection> connectionPool = new ArrayList<Connection>();
+
     private static synchronized Connection getConnection() throws Exception {
-        if (connectionPool.size() > 0) {
-            return connectionPool.remove(connectionPool.size() - 1);
-        }
-        try {
-            return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            throw new Exception(e);
-        }
+	if (connectionPool.size() > 0) {
+	    return connectionPool.remove(connectionPool.size() - 1);
+	}
+	try {
+	    return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+	} catch (SQLException e) {
+	    throw new Exception(e);
+	}
     }
-    
+
     private static synchronized void releaseConnection(Connection con) {
-        connectionPool.add(con);
+	connectionPool.add(con);
     }
-    
+
     private boolean tableExists() throws Exception {
-        Connection con = null;
-        String tableName = "user";
-        
-        try {
-            con = getConnection();
-            DatabaseMetaData metaData = con.getMetaData();
-            ResultSet rs = metaData.getTables(null, null, tableName, null);
+	Connection con = null;
+	String tableName = "user";
 
-            boolean answer = rs.next();
+	try {
+	    con = getConnection();
+	    DatabaseMetaData metaData = con.getMetaData();
+	    ResultSet rs = metaData.getTables(null, null, tableName, null);
 
-            rs.close();
-            releaseConnection(con);
+	    boolean answer = rs.next();
+	    rs.close();
+	    releaseConnection(con);
+	    return answer;
 
-            return answer;
-
-        } catch (SQLException e) {
-            try {
-                if (con != null)
-                    con.close();
-            } catch (SQLException e2) { /* ignore */
-            }
-            throw new SQLException(e);
-        }
+	} catch (SQLException e) {
+	    try {
+		if (con != null)
+		    con.close();
+	    } catch (SQLException e2) { /* ignore */
+	    }
+	    throw new SQLException(e);
+	}
     }
-    
+
     private void createUserTable() throws SQLException {
-        Connection con = null;
-        String tableName = "user";
+	Connection con = null;
+	String tableName = "user";
 
-        try {
-			con = getConnection();
-	        Statement stmt = con.createStatement();
-	        stmt.executeUpdate("CREATE TABLE " + tableName
-	        		+ " (id int not null auto_increment, "
-	        		+ "userName VARCHAR(255), "
-	        		+ "password VARCHAR(255), "
-	        		+ "firstName VARCHAR(255), "
-	        		+ "lastName VARCHAR(255), "
-	                + "PRIMARY KEY(id))");
-	        stmt.close();
-	        releaseConnection(con);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	try {
+	    con = getConnection();
+	    Statement stmt = con.createStatement();
+	    stmt.executeUpdate("CREATE TABLE " + tableName
+		    + " (id int not null auto_increment, "
+		    + "userName VARCHAR(255), "
+		    + "password VARCHAR(255), "
+		    + "firstName VARCHAR(255), "
+		    + "lastName VARCHAR(255), "
+		    + "PRIMARY KEY(id))");
+	    stmt.close();
+	    releaseConnection(con);
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
     }
-    
-    
+
+
     public User read(String userName) throws SQLException {
-        Connection con = null;
-        String tableName = "user";
-        
-        try {
-            con = getConnection();
-            PreparedStatement pstmt = con.prepareStatement("SELECT * FROM "
-                    + tableName + " WHERE userName=?");
-            pstmt.setString(1, userName);
-            ResultSet rs = pstmt.executeQuery();
+	Connection con = null;
+	String tableName = "user";
 
-            User user;
-            if (!rs.next()) {
-                user = null;
-            } else {
-                user = new User();
-                user.setUserName(rs.getString("userName"));
-                user.setPassword(rs.getString("password"));
-                user.setFirstName(rs.getString("firstName"));
-                user.setLastName(rs.getString("lastName"));
-            }
+	try {
+	    con = getConnection();
+	    PreparedStatement pstmt = con.prepareStatement("SELECT * FROM "
+		    + tableName + " WHERE userName=?");
+	    pstmt.setString(1, userName);
+	    ResultSet rs = pstmt.executeQuery();
 
-            rs.close();
-            pstmt.close();
-            releaseConnection(con);
-            return user;
+	    User user;
+	    if (!rs.next()) {
+		user = null;
+	    } else {
+		user = new User();
+		user.setUserName(rs.getString("userName"));
+		user.setPassword(rs.getString("password"));
+		user.setFirstName(rs.getString("firstName"));
+		user.setLastName(rs.getString("lastName"));
+	    }
 
-        } catch (Exception e) {
-            try {
-                if (con != null)
-                    con.close();
-            } catch (SQLException e2) { /* ignore */
-            }
-            throw new SQLException(e);
-        }
+	    rs.close();
+	    pstmt.close();
+	    releaseConnection(con);
+	    return user;
+
+	} catch (Exception e) {
+	    try {
+		if (con != null)
+		    con.close();
+	    } catch (SQLException e2) { /* ignore */
+	    }
+	    throw new SQLException(e);
+	}
     }
 }
